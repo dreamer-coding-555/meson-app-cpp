dba08f17f4af5ba2a9c26397cf97e8152496b34c
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-01-03 09:42:39 -0600

    Update xunit_runner.cpp

--- test/xunit_runner.cpp
+++ test/xunit_runner.cpp
@@ -1,22 +1,16 @@
 /*
-   File: xunit_runner.c
-   Description: This C test runner file is used for testing the Native Meson Build Application, a project under the Trilobite Coder Lab.
-   
-   Author: 
-   - Name: Michael Gene Brockus (Dreamer)
-   - Email: michaelbrockus@gmail.com
-   - Website: https://trilobite.code.blog
-
-   License: This software is released under the Apache License 2.0. Please refer to the LICENSE file for more details.
-
-   Purpose: 
-   - This C test runner file is responsible for running unit tests and verifying the correctness of the Native Meson Build Application.
-   - It should be used in conjunction with a testing framework or library to ensure the functionality of the application.
-   - Customize this file as needed for your specific project's testing requirements.
-
-   For more information on the Native Meson Build Application, the Trilobite Coder Lab project, and the testing framework or library being used, please refer to the relevant project documentation.
+==============================================================================
+Author: Michael Gene Brockus (Dreamer)
+Email: michaelbrockus@gmail.com
+Organization: Fossil Logic
+Description: 
+    This file is part of the Fossil Logic project, where innovation meets
+    excellence in software development. Michael Gene Brockus, also known as
+    "Dreamer," is a dedicated contributor to this project. For any inquiries,
+    feel free to contact Michael at michaelbrockus@gmail.com.
+==============================================================================
 */
-#include <trilobite/xtest.h>   // basic test tools
+#include <fossil/xtest.h>   // basic test tools
 
 XTEST_EXTERN_POOL(basic_group);
 
