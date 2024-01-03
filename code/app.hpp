4f0f657afb27fdb685e2e83894d94173d6efb522
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-01-03 09:50:51 -0600

    Update app.hpp

--- code/app.hpp
+++ code/app.hpp
@@ -1,42 +1,20 @@
 /*
-   Project: Native Meson Build Application
-   Description: This source file is part of the Trilobite Coder Lab project and is authored by Michael Gene Brockus (Dreamer).
-   
-   Contact Information:
-   - Email: michaelbrockus@gmail.com
-   - Website: https://trilobite.code.blog
-
-   License: This software is released under the Apache License 2.0. Please refer to the LICENSE file for more details.
-
-   This Meson build file defines the build configuration for the Native Meson Build Application.
-   It contains instructions and settings to build the project, including dependencies, compiler options,
-   and other build-related settings. Make sure to review and modify this file as needed for your specific project requirements.
-
-   For more information on Meson build system:
-   - Official website: https://mesonbuild.com/
-   - GitHub repository: https://github.com/mesonbuild/meson
-
-   Trilobite Coder Lab:
-   - Trilobite Coder Lab is a development and research initiative by Michael Gene Brockus (Dreamer).
-     It focuses on various aspects of software development, including programming, open-source projects, and code-related articles.
-     Visit the lab's website for more information and resources.
-
-   Author:
-   - Name: Michael Gene Brockus (Dreamer)
-   - Email: michaelbrockus@gmail.com
-   - Website: https://trilobite.home.blog
-
-   License:
-   - This project is licensed under the Apache License 2.0.
-     For detailed licensing information, please refer to the LICENSE file in the project directory.
-
-   For any questions, concerns, or contributions, feel free to contact the author via email or visit the Trilobite Coder Lab website.
+==============================================================================
+Author: Michael Gene Brockus (Dreamer)
+Email: michaelbrockus@gmail.com
+Organization: Fossil Logic
+Description: 
+    This file is part of the Fossil Logic project, where innovation meets
+    excellence in software development. Michael Gene Brockus, also known as
+    "Dreamer," is a dedicated contributor to this project. For any inquiries,
+    feel free to contact Michael at michaelbrockus@gmail.com.
+==============================================================================
 */
 #ifndef APP_HPP
 #define APP_HPP
 
-#include <trilobite/xcore.h> // core framework
-#include <trilobite/xutil.h> // base utilities
-#include <trilobite/xstring.h> // that string lib
+#include <fossil/xcore.h> // core framework
+#include <fossil/xutil.h> // base utilities
+#include <fossil/xstring.h> // that string lib
 
 #endif
