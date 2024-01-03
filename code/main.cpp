a7d0126f8dbf27ea257133fa489f38b09de93ff9
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-01-03 09:50:07 -0600

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -1,35 +1,15 @@
 /*
-   File: main.cpp
-   Description: This C++ source file is part of the Native Meson Build Application, which is a project under the Trilobite Coder Lab.
-   
-   Author: 
-   - Name: Michael Gene Brockus (Dreamer)
-   - Email: michaelbrockus@gmail.com
-   - Website: https://trilobite.code.blog
-
-   License: This software is released under the Apache License 2.0. Please refer to the LICENSE file for more details.
-
-   Purpose: 
-   - This C source file contains the implementation for the Native Meson Build Application.
-   - It includes the main logic and functionality required for the application to run.
-   - Review and modify this file as needed for your specific project requirements.
-
-   For more information on the Native Meson Build Application and the Trilobite Coder Lab project, please refer to the project documentation and website.
+==============================================================================
+Author: Michael Gene Brockus (Dreamer)
+Email: michaelbrockus@gmail.com
+Organization: Fossil Logic
+Description: 
+    This file is part of the Fossil Logic project, where innovation meets
+    excellence in software development. Michael Gene Brockus, also known as
+    "Dreamer," is a dedicated contributor to this project. For any inquiries,
+    feel free to contact Michael at michaelbrockus@gmail.com.
+==============================================================================
 */
-
-/*
- > Application Foundation Function:
- > This function is considered to be the starting point for applications created
- > and hosted at the Trilobite AppHub.
- > 
- > There should not be a reason for you to touch the
- > main source file; begin your work in the program
- > source file.
- >
- > @param argc The number of command-line arguments.
- > @param argv An array of strings containing the command-line arguments.
- > @return An integer indicating the application's exit status.
- */
 extern int run_app(int argc, char **argv);
 
 /*
